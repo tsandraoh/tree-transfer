4cacdf58376cf0f67d9697cd5bfd6af5acc27fa1
Sandra <nannan.tian@shihengtech.com>
2023-04-24 19:44:26 +0800

    feat: 拖拽fix

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,29 +1,49 @@
 import { memo, FC } from 'react';
-
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useDrag, DragSourceMonitor, useDrop, DropTargetMonitor, DndProvider } from 'react-dnd';
 import { Colors } from './colors_';
 import { SourceBox } from './sourceBox';
 import { StatefulTargetBox as TargetBox } from './targetBox';
 
-const Container: FC = memo(function Container() {
-  return (
-    <div style={{ overflow: 'hidden', clear: 'both', margin: '-.5rem' }}>
-      <div style={{ float: 'left' }}>
-        <SourceBox color={Colors.BLUE}>
-          <SourceBox color={Colors.YELLOW}>
-            <SourceBox color={Colors.YELLOW} />
-            <SourceBox color={Colors.BLUE} />
-          </SourceBox>
-          <SourceBox color={Colors.BLUE}>
-            <SourceBox color={Colors.YELLOW} />
-          </SourceBox>
+// export const Container: FC = memo(function Container() {
+//   return (
+//     <div style={{ overflow: 'hidden', clear: 'both', margin: '-.5rem' }}>
+//       <div style={{ float: 'left' }}>
+//         <SourceBox color={Colors.BLUE}>
+//           <SourceBox color={Colors.YELLOW}>
+//             <SourceBox color={Colors.YELLOW} />
+//             <SourceBox color={Colors.BLUE} />
+//           </SourceBox>
+//           <SourceBox color={Colors.BLUE}>
+//             <SourceBox color={Colors.YELLOW} />
+//           </SourceBox>
+//         </SourceBox>
+//       </div>
+
+//       <div style={{ float: 'left', marginLeft: '5rem', marginTop: '.5rem' }}>
+//         <TargetBox />
+//       </div>
+//     </div>
+//   );
+// });
+
+export default () => <DndProvider backend={HTML5Backend}>
+  <div style={{ overflow: 'hidden', clear: 'both', margin: '-.5rem' }}>
+    <div style={{ float: 'left' }}>
+      <SourceBox color={Colors.BLUE}>
+        <SourceBox color={Colors.YELLOW}>
+          <SourceBox color={Colors.YELLOW} />
+          <SourceBox color={Colors.BLUE} />
         </SourceBox>
-      </div>
-
-      <div style={{ float: 'left', marginLeft: '5rem', marginTop: '.5rem' }}>
-        <TargetBox />
-      </div>
+        <SourceBox color={Colors.BLUE}>
+          <SourceBox color={Colors.YELLOW} />
+        </SourceBox>
+      </SourceBox>
     </div>
-  );
-});
 
-export default Container;
+    <div style={{ float: 'left', marginLeft: '5rem', marginTop: '.5rem' }}>
+      <TargetBox />
+    </div>
+  </div>
+
+</DndProvider>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import styles from './index.less';
-import Container from './demo';
+import DrapAndDropRender from './demo';
 import SortableRender from './sortable';
 import TreetransferRender from './test/demo';
 
 export default function IndexPage() {
   return (
     <div>
-
-      {/* <div><Container /></div> */}
-    <div><SortableRender /></div> 
+      <div><DrapAndDropRender /></div>
+      <div><SortableRender /></div> 
       <div><TreetransferRender /></div>
     </div>
   );
